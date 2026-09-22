dc21603be346902c717db2afd5e8226c3b1cd900
Sara <sarazaganjori@htl.com>
2026-09-22 08:52:56 +0200

    Change th design in Register files

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -2,11 +2,53 @@
     let { form } = $props();
 </script>
 
-<div class="min-h-screen bg-purple-100 flex items-center justify-center">
+<div class="min-h-screen bg-grey-50 flex items-center justify-center p-6"> 
 
-    <div class="bg-white p-8 rounded-2xl shadow-lg w-96">
+<div class="w-full max-w-6xl bg-white rounded-2xl shadow-xl overflow-hidden grid md:grid-cols-2">
 
-        <h1 class="text-3xl font-bold text-purple-800 text-center mb-2">
+<div class="relative p-10 flex flex-col bg-cover bg-center"
+    style="background-image: url('/images/car-bg.png');">
+
+    <div class="absolute inset-0 bg-white/75"></div>   
+    <div class="relative z-10 flex flex-col h-full">
+
+    <a href="/" class="text-gray-700 hover:text-[#BF0030]">
+        ← Home Page
+    </a>
+
+    <div class="flex flex-col items-center justify-center flex-1 text-center">
+
+        <img
+            src="/images/RideExchange_logo_clean_transparent.png"
+            alt="RideExchange Logo"
+            class="w-30 mb-5"
+        />
+        <h1 class="text-5xl font-bold text-gray-900">
+            Get Started
+        </h1>
+
+        <p class="text-gray-500 text-lg mt-4">
+            Join RideExchange and hit the road together!
+        </p>
+
+        <p class="text-gray-700 mt-10">
+            Already have an account?
+        </p>
+        <a
+            href="/login"
+            class="mt-3 border-2 border-[#BF0030] text-[#BF0030]
+                   px-12 py-3 rounded-xl hover:bg-[#BF0030]
+                   hover:text-white"
+        >
+            Log in
+        </a>
+    </div>
+</div>
+</div>
+
+<div class="p-10 md:p-14">
+
+        <h1 class="text-3xl font-bold text-[#D62828] text-center mb-2">
             Register
         </h1>
 
@@ -70,25 +112,12 @@
             />
             <button
                 type="submit"
-                class="w-full bg-purple-700 text-white p-3 rounded-lg hover:bg-purple-800"
+                class="w-full bg-[#D62828] text-white p-3 rounded-lg hover:bg-[#D62828]"
             >
                 Register
             </button>
 
         </form>
-
-
-        <p class="text-center mt-5">
-            Already have an account?
-
-            <a
-                href="/login"
-                class="text-purple-700 font-bold"
-            >
-                Login
-            </a>
-        </p>
-
     </div>
-
+</div>
 </div>
