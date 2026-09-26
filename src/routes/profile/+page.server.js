87a0f8d3336c8b5df29ce4158f0b88160b4f0ba1
Sara <sarazaganjori@htl.com>
2026-09-26 20:19:14 +0200

    Add profile user details

--- src/routes/profile/+page.server.js
+++ src/routes/profile/+page.server.js
@@ -4,8 +4,14 @@ export function load({ locals }) {
     if (!locals.user) {
         throw redirect(303, '/login');
     }
-    
+
     return {
-    user: locals.user
+    user: {
+        id: locals.user.id,
+        username: locals.user.username,
+        email: locals.user.email,
+        phone_number: locals.user.phone_number,
+        role: locals.user.role
+    }
 };
 }
